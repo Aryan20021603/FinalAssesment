c7a48ecb1fcfe8deffed4c32a48dc46fc30a216f
Aryan20021603 <80641001+Aryan20021603@users.noreply.github.com>
2022-07-02 16:33:00 +0530

    Update screen3.dart

--- screen3.dart
+++ screen3.dart
@@ -10,21 +10,65 @@ class screen3 extends StatefulWidget {
 }
 
 class _screen3State extends State<screen3> {
+  DateTime today = DateTime.now();
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Padding(
-            padding: EdgeInsets.all(20),
-            child: Column(
-              children: [
-                Text(
+        child: Scaffold(
+      body: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Text(
                   "Hi ,${widget.name}",
-                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                  style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
                 ),
-                Image(
-                    image: NetworkImage(
-                        "https://i.pinimg.com/originals/e5/73/7c/e5737c44dd061635766ba1e3a4b4efb9.png")),
-              ],
-            )));
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Image(
+                  image: NetworkImage(
+                      "https://i.pinimg.com/originals/e5/73/7c/e5737c44dd061635766ba1e3a4b4efb9.png")),
+              SizedBox(
+                height: 30,
+              ),
+              Text(
+                "You Have Succesfully Completed Hybrid Mobile App Development Course.",
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Container(
+                alignment: Alignment.bottomLeft,
+                child: Text(
+                  "INSTRUCTOR NAME",
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Container(
+                alignment: Alignment.bottomLeft,
+                child: Text("Pankaj Kapoor",
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue)),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                alignment: Alignment.bottomRight,
+                child: Text("Date :${today.day}/${today.month}/${today.year}",
+                    style:
+                        TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+              )
+            ],
+          )),
+    ));
   }
 }
